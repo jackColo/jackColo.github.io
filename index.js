af96944e5f88607557d6bf78e8c81923a5db9085
Your Name <you@example.com>
2023-03-16 10:04:21 +0100

    Prima commit

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+const aggiungiPiatti = document.querySelector('.aggiungiPiatto');
+const piatti = document.querySelector('.piatti');
+const listaOrdini = JSON.parse(localStorage.getItem('ordini')) || [];
+aggiungiPiatti.addEventListener('submit', aggiungiPiatto); //posso definire le funzioni dichiarate con function perchè in fase di compilazione sono soggette ad hoisting
+aggiungiPiatti.addEventListener('button', eliminaPiatti);
+function aggiungiPiatto(e) {
+    e.preventDefault();
+    const nome = (this.querySelector('[name="piatto"]')).value;
+    const piatto = {
+        nome,
+        portato: false
+    };
+    listaOrdini.push(piatto);
+    popolaLista();
+    localStorage.setItem('ordini', JSON.stringify(listaOrdini));
+    this.reset();
+}
+function eliminaPiatti() {
+}
+function popolaLista() {
+    piatti.innerHTML = listaOrdini.map((ordine, index) => {
+        return `
+            <li>
+                <input type="checkbox" data-index="${index}" id="item${index}" ${ordine.portato ? 'checked' : ''} />
+                <label for="item${index}">${ordine.nome}</label>
+                </li>
+        `;
+    }).join('');
+}
+piatti.addEventListener('click', togglePortato); //mi metto in ascolto su ul e non su li perchè altrimenti i nuovi elementi aggiunti non avrebbero l'eventListener --> mi metto sul genitore e aspetto l'evento in risalita
+function togglePortato(e) {
+    const el = e.target;
+    if (el.matches('input')) {
+        const index = Number(el.dataset.index);
+        listaOrdini[index].portato = !listaOrdini[index].portato;
+        localStorage.setItem('ordini', JSON.stringify(listaOrdini));
+        popolaLista();
+    }
+}
+popolaLista();
